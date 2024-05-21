95758a890121dba13614dfebd3c23e4a09671bdd
sachawindmeijer <sachawindmeijer@hotmail.com>
2024-05-21 17:35:12 +0200

    error van de iconweather verholpen

--- src/helpers/iconWeather.jsx
+++ src/helpers/iconWeather.jsx
@@ -3,27 +3,27 @@ import Sunny from '../assets/icons/sun.svg';
 import Rain from '../assets/icons/rain.svg';
 import Clouds from '../assets/icons/clouds.svg';
 import Snow from '../assets/icons/snow.svg';
-import  Wind from '../assets/icons/wind.svg';
+import Wind from '../assets/icons/wind.svg';
 import Drizzle from '../assets/icons/sun-rain.svg';
 
 function iconWeather(weatherType) {
     switch (weatherType) {
         case 'Clear':
-            return <Sunny/>;
+            return <img src={Sunny} alt="Sunny icon" />;
         case 'Clouds':
-            return <Clouds/>;
+            return <img src={Clouds} alt="Clouds icon" />;
         case 'Drizzle':
-            return <Drizzle/>;
+            return <img src={Drizzle} alt="Drizzle icon" />;
         case 'Rain':
-            return <Rain/>;
+            return <img src={Rain} alt="Rain icon" />;
         case 'Snow':
-            return <Snow/>;
+            return <img src={Snow} alt="Snow icon" />;
         case 'Mist':
         case 'Haze':
         case 'Smoke':
         case 'Fog':
         default:
-            return <Wind/>;
+             return <img src={Wind} alt="Wind icon" />;//wind
     }
 }
 
